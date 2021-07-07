5167a2119724dd3fbbe6803bb48ffdc8ac2262a7
Saad Ali <saadaliyounas@gmail.com>
2021-07-07 14:58:32 +0500

    sum function added

--- src/com/example/TestClass.java
+++ src/com/example/TestClass.java
@@ -1,8 +1,15 @@
 package com.example;
 
 public class TestClass {
+    public static int sum(int a, int b){
+        if(a+b>0)
+            return a+b;
+        else
+            return 0;
+    }
     public static void main(String[] args) {
-        System.out.println("Hello!");
+        System.out.print("Sum: ");
+        System.out.println(sum(5,6));
     }
 
 }
