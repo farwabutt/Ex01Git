df6122a88fc10fdcc963fbb9e92fe262cae04527
Saad Ali <saadaliyounas@gmail.com>
2021-07-07 15:04:01 +0500

    sum function added

--- src/com/example/Prog.java
+++ src/com/example/Prog.java
@@ -2,8 +2,8 @@ package com.example;
 
 public class Prog {
 
-public static void main(String []args){
-    System.out.println("This is a test program");
-    System.out.println("Im using this to commit file in Git");
-}
+    public static void main(String []args){
+        System.out.println("This is a test program");
+        System.out.println("Im using this to commit file in Git");
+    }
 }
