0f104677c22da318ffbc1b81d640c1f1a7df5d36
Alee985 <49112854+Alee985@users.noreply.github.com>
2021-07-07 18:46:24 +0500

    HashMaps Added.

---
+++ src/com/example/HashMaps.java
@@ -0,0 +1,36 @@
+package com.example;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class HashMaps {
+    Map<String,Integer> maps;
+    HashMaps(){
+        maps=new HashMap<>();
+    }
+    public void addKeyValue(String Key,int val){ maps.put(Key,val); }
+    public int getValue(String Key){
+        return maps.get(Key);
+    }
+    public void removePair(String Key){
+        maps.remove(Key);
+    }
+    public void deleteAllRecord(){
+        maps.clear();
+    }
+    public void showRecord(){
+        System.out.println(maps);
+    }
+    public Map<String,Integer> getMaps(){
+        return maps;
+    }
+
+    public static void main(String[] args) {
+        HashMaps data = new HashMaps();
+        data.addKeyValue("Civic", 2020);
+        data.addKeyValue("Mercedes", 2018);
+        data.addKeyValue("Ferrari", 2016);
+        System.out.println("Our Showroom has the following collections with Models released as:");
+        data.showRecord();
+        }
+    }
