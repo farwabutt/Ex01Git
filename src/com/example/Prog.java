14e690dcec320e1ce38496677d2f907ee9f3c2cd
Farwa <farwa.afzaal2721@gmailcom>
2021-07-07 05:13:55 +0500

    First Git commit in Java project

---
+++ src/com/example/Prog.java
@@ -0,0 +1,9 @@
+package com.example;
+
+public class Prog {
+
+public static void main(String []args){
+    System.out.println("This is a test program");
+    System.out.println("Im using this to commit file in Git");
+}
+}
